e42f0c0a9bde12b777b442cf132d9976443ba760
MrP01 <peter@waldert.at>
2021-11-20 13:39:20 +0100

    enable search

--- config.js
+++ config.js
@@ -23,8 +23,8 @@ const config = {
         {text: 'Library', link: 'https://www.latin-is-simple.com/library/'},
     ],
     search: {
-      enabled: false,
-      indexName: '',
+      enabled: true,
+      indexName: 'latin-wiki',
       algoliaAppId: process.env.GATSBY_ALGOLIA_APP_ID,
       algoliaSearchKey: process.env.GATSBY_ALGOLIA_SEARCH_KEY,
       algoliaAdminKey: process.env.ALGOLIA_ADMIN_KEY,
